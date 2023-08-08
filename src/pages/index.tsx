594dde6b0fa638f8d4fab0f68d0a56b1b993cf81
ayakuma309 <ayako.30901@gmail.com>
2023-08-08 22:25:55 +0900

    add: pixelページ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Link href="/pixel">
+                Pixel
+            </Link>
         </main>
     );
 }
---
+++ src/pages/pixel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pixel = () => {
+  return (
+    <div>pixel</div>
+  )
+}
+
+export default pixel
